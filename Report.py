dee523148e461e2560b807d627e2447384dd091a
armandolezama <armando.lezama.psic@gmail.com>
2024-03-29 08:30:43 -0500

    feat(py): added report subsets functions

--- Report.py
+++ Report.py
@@ -1,6 +1,6 @@
 import pandas as pd
 from Report_result import Report_result
-
+from sandbox_functions import get_subsets
 class Report:
   def __init__(self, subsets:list) -> None:
     self.current_subsets = subsets
@@ -8,3 +8,11 @@ class Report:
 
   def add_subset(self, new_subset: pd.DataFrame):
     self.current_subsets = [*self.current_subsets, new_subset]
+    self.reports = [*self.reports, Report_result({'subset': new_subset })]
+
+  def generate_subsets(self, data_set, columns):
+    return get_subsets(data_set, columns=columns)
+
+  def add_multiple_subsets(self, new_subsets:list):
+    self.current_subsets = [*self.current_subsets, *new_subsets]
+    self.reports = [*self.reports, *[Report_result({'subset': dataset }) for dataset in new_subsets]]
